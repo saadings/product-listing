8e21a36dd0b3f74ba5921fe1403a5f01a5429873
saadings <saadijutt007@gmail.com>
2024-01-02 16:04:26 +0500

    Updated CRON Job

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const prisma = new PrismaClient();
+
+    const products = await prisma.product.findMany();
+
+    const updatePromises = products.map((product) => {
+      const randomPrice = Math.floor(Math.random() * 1000) + 1; // Random price between 1 and 1000
+      return prisma.product.update({
+        where: { id: product.id },
+        data: { price: randomPrice },
+      });
+    });
+
+    await Promise.all(updatePromises);
+
+    await prisma.$disconnect();
+
+    return Response.json({ message: "Cron job executed." });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
